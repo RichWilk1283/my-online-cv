2e045abc72f66b0d6db53aa32ac9487d4b070485
Richard Wilkinson <richardwilkinson1283@gmail.com>
2024-04-23 19:48:46 +0100

    Changed the depricated images.domains config for current settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ["cloud.appwrite.io","fontawesome.com"],
+    remotePatterns: [
+      { hostname: "cloud.appwrite.io" },
+      { hostname: "fontawesome.com" },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
