24ca70d4016c91a45c69ca8b83487839387a2163
Richard Wilkinson <richardwilkinson1283@gmail.com>
2024-06-14 14:51:37 +0100

    Updated next.config.js with basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/my-online-cv" : "",
+  assetPrefix: isProd ? "/my-online-cv" : "",
   output: "export",
   distDir: "dist",
   images: {
