1ce2e6730237b2bf30cbb4a8ce71bbbe89aada10
Richard Wilkinson <richardwilkinson1283@gmail.com>
2024-06-16 15:35:22 +0100

    Removed some next.config.js settings in prep for deploying to vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? "/my-online-cv" : "",
-  assetPrefix: isProd ? "/my-online-cv" : "",
-  output: "export",
-  distDir: "dist",
   images: {
-    unoptimized: true,
     remotePatterns: [
       { hostname: "cloud.appwrite.io" },
       { hostname: "fontawesome.com" },
